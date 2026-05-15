0272b6e607518d4b1650d021329fd545c1a047be
Chiemela <onyensochiemela24@gmail.com>
2026-05-15 00:59:46 -0500

    Connect TextField to WeatherController

--- src/main/java/com/chiemela/javafxweatherapp/WeatherController.java
+++ src/main/java/com/chiemela/javafxweatherapp/WeatherController.java
@@ -2,10 +2,15 @@ package com.chiemela.javafxweatherapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class WeatherController {
     @FXML
+    private TextField cityInput;
+
+    @FXML
     private void getWeather() {
-        System.out.println("Button clicked");
+        String city = cityInput.getText();
+        System.out.println("Button clicked: " + city);
     }
 }
